85f1312bece7dadd76066ef4c77d3f05e6755516
JamesETrent <Jetscout2014@gmail.com>
2023-10-07 03:32:40 -0400

    openAI import

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import semantic_kernel as sk
 kernel = sk.Kernel()
+
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+
+api_key, org_id = sk.openai_settings_from_dot_env()
+
+kernel.add_chat_service("chat-gpt", OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id))
+
