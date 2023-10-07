c093e4c1721437d4aa8a8c04e17027f12c190998
Maverick Reynolds <mcreynolds02@gmail.com>
2023-10-07 04:26:00 -0400

    next

--- morgan-ai/src/App.js
+++ morgan-ai/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // State to store the user input
+  const [inputText, setInputText] = useState('');
+  // State to store the submitted text for display
+  const [submittedText, setSubmittedText] = useState('');
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Set the submitted text to the input text
+    setSubmittedText(inputText);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Form Example</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter Text:
+          <input
+            type="text"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {submittedText && (
+        <div>
+          <h2>Submitted Text:</h2>
+          <p>{submittedText}</p>
+        </div>
+      )}
     </div>
   );
 }
