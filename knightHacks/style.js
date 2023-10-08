30d199ad90d1f3c275b41a119c151a679b7dd6bb
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-10-07 20:05:20 -0400

    Update style.js

--- knightHacks/style.js
+++ knightHacks/style.js
@@ -2,35 +2,37 @@ let slideIndex = 1; // start on first slide
 showSlide(slideIndex);
 
 function moveSlide(n) {
-    showSlide(slideIndex += n);
+  showSlide((slideIndex += n));
 }
 
 function showSlide(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].classList.remove("active");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-  slides[slideIndex-1].classList.add("active");
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[slideIndex - 1].classList.add("active");
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   let prevButton = document.querySelector(".prev");
   let nextButton = document.querySelector(".next");
-  
-  prevButton.addEventListener("click", function() {
-      moveSlide(-1);
+
+  prevButton.addEventListener("click", function () {
+    moveSlide(-1);
   });
 
-  nextButton.addEventListener("click", function() {
-      moveSlide(1);
+  nextButton.addEventListener("click", function () {
+    moveSlide(1);
   });
 });
 
-let slideInterval = setInterval(function() {
+let slideInterval = setInterval(function () {
   moveSlide(1);
 }, 5000);
-
-
