a95e3e083a0dbad3a4553eb13268928550839bf1
Richard Brito <richardbrito116@gmail.com>
2024-01-18 22:40:37 -0500

    added ai key, made .env made .gitignore
    updated ai key, made .env made .gitignore

--- knightHacks/style.js
+++ knightHacks/style.js
@@ -1,3 +1,22 @@
+import dotEnv from "dotenv";
+ai = require("apiai")(process.env.APIAI_TOKEN);
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    //messages: [{"role": "system", "content": "You are a helpful assistant."},
+      //  {"role": "user", "content": "Who won the world series in 2020?"},
+        //{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        //{"role": "user", "content": "Where was it played?"}],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+main();
+
 let slideIndex = 1; // start on first slide
 showSlide(slideIndex);
 
